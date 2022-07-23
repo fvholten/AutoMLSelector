006e630b21883ca3ee95004149f4fb244b63b52e
Finn von Holten <finn.eric.von.holten@gmail.com>
2022-07-23 13:34:28 +0200

    build main.py automl-tool

---
+++ app/main.py
@@ -0,0 +1,47 @@
+import _pickle as cPickle
+import csv
+import sys
+import openml as oml
+import pandas as pd
+import logging
+
+with open(r"_meta.csv") as csv_file:
+  csv_reader = csv.reader(csv_file, delimiter=',')
+  list_qualities = list()
+  for line in csv_reader:
+    list_qualities.append(line[0])
+  list_qualities = list_qualities[1:]
+
+with open(r"ml-models.pickle", "rb") as input_file:
+  ml_models = cPickle.load(input_file)
+
+
+dataset_id = sys.argv[1]
+dataset = oml.datasets.get_dataset(dataset_id)
+
+logging.info('Loaded dataset:' + dataset.name)
+
+t = dict()
+
+for quality in list_qualities:
+  t[quality] = [dataset.qualities.get(quality)]
+test_df = pd.DataFrame(t)
+
+bestProbability = dict()
+currentBest = 0
+for key, value in ml_models.items():
+  aml = value.get('model')
+  probability = aml.predict_proba(test_df)[0]
+  logging.info("{key}: {probability}".format(key=key, probability=probability))
+
+  probability1 = float(probability[1])
+
+  if probability1 == currentBest:
+    bestProbability[key] = probability1
+
+  if probability1 > currentBest:
+    currentBest = probability1
+    bestProbability.clear()
+    bestProbability[key] = probability1
+    
+print("The best-choice for:\n\"{dataset}\"\n\nis the AutoML-Tool: \"{automl_tool}\".".format(dataset=dataset, automl_tool=', '.join(bestProbability)))
