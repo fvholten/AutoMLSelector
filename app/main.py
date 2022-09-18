f3068984330a12e2c7ddd9b22d4f901e99da3e72
Finn von Holten <finn.eric.von.holten@gmail.com>
2022-09-18 19:09:52 +0200

    shorter code

--- app/main.py
+++ app/main.py
@@ -1,58 +1,28 @@
-import pickle
-import csv
-import sys
-import openml as oml
-import pandas as pd
-import logging
-from pathlib import Path
+import pickle , csv , sys , logging , pandas , openml, pathlib
 
 def predict(ml_models, test_df):
-  bestProbability = dict()
-  currentBest = 0
-  for key, value in ml_models.items():
-    aml = value.get('model')
-    probability = aml.predict_proba(test_df)[0]
-    logging.info("{key}: {probability}".format(key=key, probability=probability))
+  classes_to_proba = {k: v.get('model').predict_proba(test_df)[0][1] for k, v in ml_models.items()}
+  best = max(classes_to_proba, key=classes_to_proba.get)
+  return classes_to_proba.get(best), best
 
-    probability1 = float(probability[1])
-    if probability1 == currentBest:
-      bestProbability[key] = probability1
-
-    if probability1 > currentBest:
-      currentBest = probability1
-      bestProbability.clear()
-      bestProbability[key] = probability1
-  return currentBest, ' or '.join(bestProbability)
-
-curr_dir = Path(__file__).parent
+curr_dir = pathlib.Path(__file__).parent
 
 with open(curr_dir.joinpath(r'_meta.csv')) as csv_file:
   csv_reader = csv.reader(csv_file, delimiter=',')
-  list_qualities = list()
-  for line in csv_reader:
-    list_qualities.append(line[0])
-  list_qualities = list_qualities[1:]
+  list_qualities = [line[0] for line in csv_reader][1:]
 
 with open(curr_dir.joinpath(r'ml-models.pickle'), "rb") as input_file:
   ml_models = pickle.load(input_file)
 
-
-dataset_id = sys.argv[1]
-dataset = oml.datasets.get_dataset(dataset_id)
-
+dataset = openml.datasets.get_dataset(dataset_id=sys.argv[1])
 logging.info('Loaded dataset:' + dataset.name)
 
-t = dict()
-
-for quality in list_qualities:
-  t[quality] = [dataset.qualities.get(quality)]
-test_df = pd.DataFrame(t)
+test_df = pandas.DataFrame({ quality : [dataset.qualities.get(quality)] for quality in list_qualities})
 
 proba_of_best, automl_tool = predict(ml_models, test_df)
 
 print("For dataset:")
 print(dataset)
 print()
-print()
-print("The best-choice is: {automl_tool}".format(automl_tool=automl_tool))
-print("-> with a probability of: {proba}".format(proba=proba_of_best))
+print("The best-choice is: {}".format(automl_tool))
+print("-> with a probability of: {}".format(proba_of_best))
