0be94ee74bf504a88f4d25f2072560ea52af3b1f
Finn von Holten <finn.eric.von.holten@gmail.com>
2022-09-18 19:15:05 +0200

    refactoring

--- app/main.py
+++ app/main.py
@@ -1,43 +1,27 @@
-import pickle
-import csv
-import sys
-import openml as oml
-import pandas as pd
-import logging
-from pathlib import Path
+import pickle , csv , sys , logging , pandas , openml, pathlib
 
 def predict(automl, test_df):
     classes_to_proba = dict(zip(automl.classes_, automl.predict_proba(test_df)[0]))
     return classes_to_proba.get(automl.predict(test_df)[0]),automl.predict(test_df)[0]
     
-curr_dir = Path(__file__).parent
+curr_dir = pathlib.Path(__file__).parent
 
 with open(curr_dir.joinpath(r'_meta.csv')) as csv_file:
   csv_reader = csv.reader(csv_file, delimiter=',')
-  list_qualities = list()
-  for line in csv_reader:
-    list_qualities.append(line[0])
-  list_qualities = list_qualities[1:]
+  list_qualities = [line[0] for line in csv_reader][1:]
 
 with open(curr_dir.joinpath(r'automl.pkl'), "rb") as input_file:
   automl = pickle.load(input_file)
 
-dataset_id = sys.argv[1]
-dataset = oml.datasets.get_dataset(dataset_id)
-
+dataset = openml.datasets.get_dataset(dataset_id=sys.argv[1])
 logging.info('Loaded dataset:' + dataset.name)
 
-t = dict()
-
-for quality in list_qualities:
-  t[quality] = [dataset.qualities.get(quality)]
-test_df = pd.DataFrame(t)
+test_df = pandas.DataFrame({ quality : [dataset.qualities.get(quality)] for quality in list_qualities})
 
 proba_of_best, automl_tool = predict(automl, test_df)
 
 print("For dataset:")
 print(dataset)
 print()
-print()
-print("The best-choice is: {automl_tool}".format(automl_tool=automl_tool))
-print("-> with a probability of: {proba}".format(proba=proba_of_best))
+print("The best-choice is: {}".format(automl_tool))
+print("-> with a probability of: {}".format(proba_of_best))
