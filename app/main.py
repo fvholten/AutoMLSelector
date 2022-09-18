f121b60d8ffdabe94c04de383ec224ff33e485ea
Finn von Holten <finn.eric.von.holten@gmail.com>
2022-09-18 17:52:50 +0200

    refactoring

--- app/main.py
+++ app/main.py
@@ -6,6 +6,24 @@ import pandas as pd
 import logging
 from pathlib import Path
 
+def predict(ml_models, test_df):
+  bestProbability = dict()
+  currentBest = 0
+  for key, value in ml_models.items():
+    aml = value.get('model')
+    probability = aml.predict_proba(test_df)[0]
+    logging.info("{key}: {probability}".format(key=key, probability=probability))
+
+    probability1 = float(probability[1])
+    if probability1 == currentBest:
+      bestProbability[key] = probability1
+
+    if probability1 > currentBest:
+      currentBest = probability1
+      bestProbability.clear()
+      bestProbability[key] = probability1
+  return currentBest, ' or '.join(bestProbability)
+
 curr_dir = Path(__file__).parent
 
 with open(curr_dir.joinpath(r'_meta.csv')) as csv_file:
@@ -30,21 +48,11 @@ for quality in list_qualities:
   t[quality] = [dataset.qualities.get(quality)]
 test_df = pd.DataFrame(t)
 
-bestProbability = dict()
-currentBest = 0
-for key, value in ml_models.items():
-  aml = value.get('model')
-  probability = aml.predict_proba(test_df)[0]
-  logging.info("{key}: {probability}".format(key=key, probability=probability))
+proba_of_best, automl_tool = predict(ml_models, test_df)
 
-  probability1 = float(probability[1])
-
-  if probability1 == currentBest:
-    bestProbability[key] = probability1
-
-  if probability1 > currentBest:
-    currentBest = probability1
-    bestProbability.clear()
-    bestProbability[key] = probability1
-
-print("For dataset:\n\"{dataset}\"\n\nThe best-choice is: \"{automl_tool}\"\n-> with a probability of: {proba}".format(dataset=dataset, automl_tool=', '.join(bestProbability), proba=currentBest))
+print("For dataset:")
+print(dataset)
+print()
+print()
+print("The best-choice is: {automl_tool}".format(automl_tool=automl_tool))
+print("-> with a probability of: {proba}".format(proba=proba_of_best))
